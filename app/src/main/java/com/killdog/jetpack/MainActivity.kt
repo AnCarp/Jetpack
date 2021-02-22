cf76a17cd658e1de5c8d7b9014ceed883e6297cf
WhoseThousandDollars <1543740100@qq.com>
2021-02-22 13:46:26 +0800

    viewBinding1

--- app/src/main/java/com/killdog/jetpack/MainActivity.kt
+++ app/src/main/java/com/killdog/jetpack/MainActivity.kt
@@ -2,10 +2,25 @@ package com.killdog.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.killdog.jetpack.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel= ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.button.setOnClickListener {
+            viewModel.counter++
+            refreshCounter()
+        }
+        refreshCounter()
+    }
+    private fun refreshCounter(){
+        binding.textView.text=viewModel.counter.toString()
     }
 }
