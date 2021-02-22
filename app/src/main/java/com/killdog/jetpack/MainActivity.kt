992185044e4dd7dc67cdcd1cf89916d01acefea1
WhoseThousandDollars <1543740100@qq.com>
2021-02-22 15:44:02 +0800

    viewBinding1

--- app/src/main/java/com/killdog/jetpack/MainActivity.kt
+++ app/src/main/java/com/killdog/jetpack/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.killdog.jetpack
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.lifecycle.ViewModelProvider
 import com.killdog.jetpack.databinding.ActivityMainBinding
 
@@ -9,18 +12,33 @@ import com.killdog.jetpack.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
     lateinit var binding:ActivityMainBinding
+    lateinit var sp: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel= ViewModelProvider(this).get(MainViewModel::class.java)
-        binding.button.setOnClickListener {
+        sp=getPreferences(Context.MODE_PRIVATE)
+        val countReserved=sp.getInt("restart",0)
+        viewModel= ViewModelProvider(this,MainViewModelFactory(countReserved)).get(MainViewModel::class.java)
+        binding.button.setOnClickListener {//计时器
             viewModel.counter++
+            refreshCounter()
+        }
+        binding.button2.setOnClickListener {
+            viewModel.counter=0
             refreshCounter()
         }
         refreshCounter()
     }
+
     private fun refreshCounter(){
         binding.textView.text=viewModel.counter.toString()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sp.edit{
+            putInt("count",viewModel.counter)
+        }
     }
 }
